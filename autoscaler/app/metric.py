46e289adfd5191dfdf87122a3b5bd3219ad42d34
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2022-07-08 12:32:02 +0300

    Implement reactive scaling simulator (#4)

--- autoscaler/app/metric.py
+++ autoscaler/app/metric.py
@@ -1,17 +1,24 @@
-from typing import List
+from typing import List, NamedTuple
 
 import numpy as np
 
 import autoscaler.app.app as app
 
 
+class WindowBundle(NamedTuple):
+    used_resrc_vals: np.ndarray
+    used_resrc_ratios: np.ndarray
+
+
 class MetriCollector:
     _a: app.App
     _used_resrc_vals: List[int]
+    _used_resrc_ratios: List[float]
     _curr_time: int
     
     def __init__(self, a: app.App) -> None:
         self._used_resrc_vals = []
+        self._used_resrc_ratios = []
         self._a = a
         self._curr_time = -1
 
@@ -20,17 +27,25 @@ class MetriCollector:
         for i in range(self._curr_time+1, t+1):
             used_resrc = self._a.used_resrc(i)
             self._used_resrc_vals.append(used_resrc)
+            
+            used_resrc_ratio = self._a.used_resrc_ratio(i)
+            self._used_resrc_ratios.append(used_resrc_ratio)
 
         self._curr_time = t
 
     # the window is [start; end)
-    def window(self, start: int, end: int) -> np.ndarray:
+    def window(self, start: int, end: int) -> WindowBundle:
         vals = np.zeros(end-start)
+        ratios = np.zeros(end-start)
         
         if len(self._used_resrc_vals) < end:
             raise Exception(f"fetching uncollected window [{start};{end})")
 
         for t in range(start, end):
             vals[t] = self._used_resrc_vals[t]
+            ratios[t] = self._used_resrc_ratios[t]
 
-        return vals
+        return WindowBundle(
+            used_resrc_vals=vals,
+            used_resrc_ratios=ratios,
+        )
---
+++ autoscaler/simulator/simulator.py
@@ -0,0 +1,110 @@
+import logging
+import random
+from typing import List, Tuple
+
+import numpy as np
+
+import autoscaler.app as app
+import autoscaler.converger as cvg
+
+
+class Metrics:
+    concept_drift_detection_ratio: float
+    relative_loss: float
+
+
+class Simulator:
+    _LOW_AMPLI = 0.1
+    _HIGH_AMPLI = 2
+    _LOW_FREQ = 0.02
+    _HIGH_FREQ = 2
+    _LOW_PHASE = 0.0
+    _HIGH_PHASE = np.pi * 2
+    
+    _a: app.App
+    _converger: cvg.Converger
+    _metri: app.MetriCollector
+    _cons_count: int
+    _moment: int
+    
+    def __init__(
+        self, 
+        a: app.App, 
+        converger: cvg.Converger,
+        metri: app.MetriCollector
+    ) -> None:
+        self._a = a
+        self._converger = converger
+        self._metri = metri
+
+        self._cons_count = 0
+        self._moment = 0
+        
+        self._add_consumers(upto=4)
+
+    def run(self, num_seasons: int) -> None:
+        for season in range(num_seasons):
+            logging.info(f"running season {season}")
+
+            self._run_season()
+
+    def _run_season(self) -> None:
+        self._rem_consumers()
+        self._add_consumers()
+            
+        for _ in range(app.N):
+            self._converger.converge(self._moment)
+            self._metri.collect(self._moment)
+
+            self._moment += 1
+
+    def _rem_consumers(self, upto: int = 2) -> None:
+        num = random.randint(0, upto)
+        names = set(self._a.consumer_names())
+
+        for _ in range(num):
+            if len(names) <= 0:
+                break
+            
+            names.remove(random.choice(tuple(names)))            
+
+    def _add_consumers(self, upto: int = 2) -> None:
+        num = random.randint(0, upto)
+        consumers = self._gen_consumers(num)
+
+        for consumer in consumers:
+            self._a.add_consumer(f"{self._cons_count}", consumer)
+            self._cons_count += 1
+
+    def _gen_consumers(self, num: int) -> List[app.Consumer]:
+        consumers = [None] * num
+        
+        for i in range(num):
+            consumers[i] = self._gen_consumer()
+
+        return consumers
+
+    def _gen_consumer(self) -> app.Consumer:
+        side = random.randint(0, 1)
+        
+        if side == 0:
+            return self._gen_sin_consumer()
+
+        return self._gen_cos_consumer()
+
+    def _gen_sin_consumer(self) -> app.Consumer:
+        ampli, freq, phase = self._generate_random_consumer_cfg()
+
+        return app.SinConsumer(ampli, freq, phase)
+
+    def _gen_cos_consumer(self) -> app.Consumer:
+        ampli, freq, phase = self._generate_random_consumer_cfg()
+        
+        return app.CosConsumer(ampli, freq, phase)
+
+    def _generate_random_consumer_cfg(self) -> Tuple[float, float, float]:
+        ampli = self._LOW_AMPLI + random.random() * (self._HIGH_AMPLI - self._LOW_AMPLI)
+        freq = self._LOW_FREQ + random.random() * (self._HIGH_FREQ - self._LOW_FREQ)
+        phase = self._LOW_PHASE + random.random() * (self._HIGH_PHASE - self._LOW_PHASE)
+
+        return ampli, freq, phase
